e41c0b4e8f5964947f988e694ef2d65e6e4d8a4b
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-11-15 14:40:36 +0000

    Profile

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,33 @@
-import { Row, Col, Container, Button, Image } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Container,
+  Button,
+  Image,
+  FormControl,
+} from "react-bootstrap";
 import "./sidebar-style.css";
 import { FiArrowLeft } from "react-icons/fi";
+import { FaPen } from "react-icons/fa";
+import image from "../../data/Capture1.PNG";
 
 const Main = () => {
+  // const profileSidebar = ;
+
   const showProfile = () => {
-    console.log("profile");
+    console.log("clicked");
+    document
+      .getElementsByClassName("sidebar-bt")[0]
+      .classList.add("profile-open");
+    document.getElementsByClassName("picIm")[0].classList.add("pic-height");
+  };
+
+  const hideProfile = () => {
+    console.log("clicked");
+    document
+      .getElementsByClassName("sidebar-bt")[0]
+      .classList.remove("profile-open");
+    document.getElementsByClassName("picIm")[0].classList.remove("pic-height");
   };
 
   return (
@@ -12,33 +35,47 @@ const Main = () => {
       <Row>
         <Col xs={4}>
           {/* <Button onclick={showProfile}> SHOW</Button> */}
-          <Row className="sidebar-bt">
+          <Row className="sidebar-bt profile-container">
             <Col>
               <Row className="bg-1 px-3">
                 <div className="profile">
-                  <FiArrowLeft />
+                  <FiArrowLeft
+                    className="arrow"
+                    onClick={() => hideProfile()}
+                  />
                   <span className="pl-3"> Profile</span>
                 </div>
               </Row>
-              <Row className="bg-2 px-3">
-                <div>
-                  <Image
-                    //   className="profileimagesmall"
-                    src={this.state.user.image}
-                    roundedCircle
-                  />
+              <Row className="bg-2 px-3 d-flex flex-column">
+                <div className="mx-auto pic">
+                  <img className="picIm" src={image} alt="" />
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+
+                <div className="input-heading">Your name</div>
+                <input
+                  className="formStyle"
+                  value="Barry"
+                  aria-describedby="basic-addon1"
+                />
+                <div className="text-muted">
+                  This is not your username or pin. This name will be visible to
+                  your WhatsApp contacts
+                </div>
+                <div className="input-heading">About</div>
+                {/* <div className="input">Hey there, I am using WhatsApp</div> */}
+                <input
+                  className="formStyle"
+                  value="Hey there, I am using WhatsApp"
+                  aria-describedby="basic-addon1"
+                />
               </Row>
             </Col>
           </Row>
           <Row></Row>
         </Col>
-        <Col xs={8}></Col>
+        <Col xs={8}>
+          <Button onClick={() => showProfile()}> SHOW</Button>
+        </Col>
       </Row>
     </Container>
   );
