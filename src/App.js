d1bd3164290c8a8f13d979740106ace7ee04150d
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-11-15 11:42:53 +0000

    dtart

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ import Main from "./components/Main/Main";
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div>
       <Route path="/Login" exact component={Login} />
       <Route path="/Register" exact component={Register} />
       <Route path="/Main/:id" component={Main} />
     </div>
+=======
+    <Router>
+      <div className="App">
+        <Route path="/Login" exact component={Login} />
+        <Route path="/Register" exact component={Register} />
+        <Route path="/Main" component={Main} />
+      </div>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,44 @@
+<<<<<<< Updated upstream
+=======
+import { Row, Col, Container, Button } from "react-bootstrap";
+import "./sidebar-style.css";
+import { FiArrowLeft } from "react-icons/fi";
+
+>>>>>>> Stashed changes
 const Main = () => {
+  const showProfile = () => {
+    console.log("profile");
+  };
+
   return (
     <Container>
       <Row>
         <Col xs={4}>
-          <Row></Row>
+          {/* <Button onclick={showProfile}> SHOW</Button> */}
+          <Row className="sidebar-bt">
+            <Col>
+              <Row className="bg-1 px-3">
+                <div className="profile">
+                  <FiArrowLeft />
+                  <span className="pl-3"> Profile</span>
+                </div>
+              </Row>
+              <Row className="bg-2 px-3">
+                <div>
+                  <Image
+                    //   className="profileimagesmall"
+                    src={this.state.user.image}
+                    roundedCircle
+                  />
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </Row>
+            </Col>
+          </Row>
           <Row></Row>
         </Col>
         <Col xs={8}></Col>
