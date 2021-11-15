35bea8a78efa4882bcfcc556f9d2065798d60fc3
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-11-15 10:23:03 +0000

    Start frontend routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Route path="/Login" exact component={Login} />
+      <Route path="/Register" exact component={Register} />
+      <Route path="/Main/:id" component={Main} />
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={4}>
+          <Row></Row>
+          <Row></Row>
+        </Col>
+        <Col xs={8}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Main;
