18bc68647ecc3cd177795c152f23bc0e15e56777
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-11-18 11:54:51 +0000

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,27 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Image } from "react-bootstrap";
 import { FiArrowLeft } from "react-icons/fi";
 // import image from "../../data/Capture1.PNG";
 import MessageChat from "./chatMessages/MessageChat";
 import "./sidebar-style.css";
 import ChatList from "./ChatList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Main = () => {
-  // const profileSidebar = ;
+  const [profile, setProfile] = useState({});
+  const [imageUploaded, setimageUploaded] = useState(false);
+  const [imageFile, setimageFile] = useState(
+    "https://res.cloudinary.com/btrearty/image/upload/v1637232496/avatar/oitc3d8ldczeli6bmvdt.png"
+  );
+  // const [imagePreview, setimagePreview] = useState(
+  //   "https://res.cloudinary.com/btrearty/image/upload/v1637232496/avatar/oitc3d8ldczeli6bmvdt.png"
+  // );
 
   const showProfile = () => {
     console.log("clicked");
     document
       .getElementsByClassName("sidebar-bt")[0]
       .classList.add("profile-open");
-    // document.getElementsByClassName("picIm")[0].classList.add("pic-height");
+    document.getElementsByClassName("picIm")[0].classList.add("pic-height");
   };
 
   const hideProfile = () => {
@@ -22,7 +29,46 @@ const Main = () => {
     document
       .getElementsByClassName("sidebar-bt")[0]
       .classList.remove("profile-open");
-    // document.getElementsByClassName("picIm")[0].classList.remove("pic-height");
+    document.getElementsByClassName("picIm")[0].classList.remove("pic-height");
+  };
+
+  const getMyProfile = async () => {
+    try {
+      let response = await fetch(`http://localhost:3003/users/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkzYTI5NDEyNTdjZjU5ZWM3MDRhMWEiLCJpYXQiOjE2MzcyMjk2MjAsImV4cCI6MTYzNzMxNjAyMH0.fd61ct7VmoE3LPkddZR5mUza2yh68qgAkNiF7qbF_Sg`,
+          cookie: `accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkzYTI5NDEyNTdjZjU5ZWM3MDRhMWEiLCJpYXQiOjE2MzcyMjk2MjAsImV4cCI6MTYzNzMxNjAyMH0.fd61ct7VmoE3LPkddZR5mUza2yh68qgAkNiF7qbF_Sg; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkzYTI5NDEyNTdjZjU5ZWM3MDRhMWEiLCJpYXQiOjE2MzcyMjk2MjAsImV4cCI6MTYzNzgzNDQyMH0.0vnJrJBd-QMKCkeJbgFDNQ6nFrarMI1PpE3w9dAX_iI`,
+        },
+      });
+      let myProfile = await response.json();
+      setProfile(myProfile);
+      console.log(profile);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("image", imageFile);
+    console.log(formData);
+    try {
+      let response = await fetch(`http://localhost:3003/users/me/avatar`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkzYTI5NDEyNTdjZjU5ZWM3MDRhMWEiLCJpYXQiOjE2MzcyMjk2MjAsImV4cCI6MTYzNzMxNjAyMH0.fd61ct7VmoE3LPkddZR5mUza2yh68qgAkNiF7qbF_Sg`,
+          cookie: `accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkzYTI5NDEyNTdjZjU5ZWM3MDRhMWEiLCJpYXQiOjE2MzcyMjk2MjAsImV4cCI6MTYzNzMxNjAyMH0.fd61ct7VmoE3LPkddZR5mUza2yh68qgAkNiF7qbF_Sg; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkzYTI5NDEyNTdjZjU5ZWM3MDRhMWEiLCJpYXQiOjE2MzcyMjk2MjAsImV4cCI6MTYzNzgzNDQyMH0.0vnJrJBd-QMKCkeJbgFDNQ6nFrarMI1PpE3w9dAX_iI`,
+        },
+      });
+      if (response.ok) {
+        const imageUploadResponse = await response.json();
+        console.log(imageUploadResponse);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // useEffect(() => {
@@ -31,6 +77,8 @@ const Main = () => {
   //     console.log(`Connection ${newUser}!`);
   //   });
   // });
+
+  useEffect(() => getMyProfile(), []);
 
   return (
     <Container fluid className="h-100">
@@ -51,13 +99,41 @@ const Main = () => {
               </Row>
               <Row className="bg-2 px-3 d-flex flex-column">
                 <div className="mx-auto pic">
-                  {/* <img className="picIm" src={image} alt="" /> */}
+                  {/* <img className="picIm" src={profile.image} alt="" /> */}
+                  {imageUploaded ? (
+                    // <Image src={imageFile} fluid />
+                    // <img className="picIm" src={profile.image} alt="" />
+                    <img className="picIm" src={imageFile} alt="" />
+                  ) : (
+                    <div className="text-center m-auto">
+                      <div className="m-auto imageuploadtext text-primary px-2 pt-1">
+                        <label
+                          for="file-upload"
+                          className="custom-file-upload py-2"
+                        >
+                          Select image
+                        </label>
+
+                        <input
+                          className="d-none"
+                          id="file-upload"
+                          type="file"
+                          onChange={(e) => {
+                            setimageFile(e);
+                            uploadImage();
+                          }}
+                        />
+                      </div>
+                      {/* <img className="picIm" src={profile.image} alt="" /> */}
+                      <img className="picIm" src={imageFile} alt="" />
+                    </div>
+                  )}
                 </div>
 
                 <div className="input-heading">Your name</div>
                 <input
                   className="formStyle"
-                  value="Barry"
+                  value={profile.username}
                   aria-describedby="basic-addon1"
                 />
                 <div className="text-muted">
