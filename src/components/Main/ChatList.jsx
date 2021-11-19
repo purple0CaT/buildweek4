a90ed6bff0942596d8cf684d401ec974f8366087
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-11-19 14:31:05 +0000

    problemo

--- src/components/Main/ChatList.jsx
+++ src/components/Main/ChatList.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, InputGroup, FormControl } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { connect } from "react-redux";
-import { setActiveChat } from "../../redux/actions/action.js";
+import { useSelector } from "react-redux";
+import { setActiveChat, setChats } from "../../redux/actions/action.js";
 
 const mapStateToProps = (state) => ({
   chats: state.chats.list,
@@ -10,14 +12,22 @@ const mapDispatchToProps = (dispatch) => ({
   selectChat: (chat) => {
     dispatch(setActiveChat(chat));
   },
+  // setChats: (array)
 });
 
-const ChatList = ({ selectChat }) => {
+const ChatList = ({ selectChat, chats }) => {
   // STATE & useEffect FOR TESTING
-  const [chats, setChats] = useState([]);
+  // const [chats, setChats] = useState([]);
   // const getFakeChats = () => {
   //   setChats(tempChatExample);
   // };
+  const dispatch = useDispatch();
+  const [searchUsers, setSearchUsers] = useState([]);
+  const [showModel, setShowModel] = useState(false);
+  const userId = useSelector((state) => state.userInfo._id);
+  const toggleModel = () => {
+    setShowModel(!showModel);
+  };
 
   const getRealChats = async () => {
     try {
@@ -27,7 +37,52 @@ const ChatList = ({ selectChat }) => {
       if (response.ok) {
         const chatsRes = await response.json();
         console.log(chatsRes);
-        setChats(chatsRes);
+        dispatch(setChats(chatsRes));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUsers = async (query) => {
+    try {
+      let response = await fetch(
+        `http://localhost:3003/users/search/${query}`,
+        {
+          credentials: "include",
+        }
+      );
+      if (response.ok) {
+        const usersRes = await response.json();
+        console.log(usersRes);
+        setSearchUsers(usersRes);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createNewChat = async (receiverId) => {
+    const obj = {
+      name: "All the lads3",
+      image: "",
+    };
+    try {
+      let response = await fetch(
+        `http://localhost:3003/chats/createChat/${receiverId}`,
+        {
+          method: "POST",
+          body: JSON.stringify(obj),
+          credentials: "include",
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (response.ok) {
+        const newChatRes = await response.json();
+        console.log(newChatRes);
+        dispatch(setChats(newChatRes));
+        // setSearchUsers(usersRes);
       }
     } catch (error) {
       console.log(error);
@@ -44,13 +99,41 @@ const ChatList = ({ selectChat }) => {
       className="bg-dark text-white"
       style={{ maxHeight: "calc(100vh - 3rem)", overflowY: "scroll" }}
     >
-      <h1>Chat List</h1>
+      <h1 onClick={toggleModel} style={{ cursor: "pointer" }}>
+        Users
+      </h1>
+      {showModel ? (
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="inputGroup-sizing-default">
+            Default
+          </InputGroup.Text>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+            onChange={(e) => getUsers(e.target.value)}
+          />
+        </InputGroup>
+      ) : (
+        ""
+      )}
+      {searchUsers.length > 0 &&
+        searchUsers.map((user) => (
+          <p>
+            <strong
+              onClick={() => createNewChat(user._id)}
+              style={{ cursor: "pointer" }}
+            >
+              {user.username}
+            </strong>
+          </p>
+        ))}
+      {console.log(chats)}
       {chats &&
         chats.map((chat) => (
           <Row key={chat._id} onClick={() => selectChat(chat)}>
             {console.log(chat)}
             <Col xs={2}>
-              {chat.members && chat.members.length === 1 ? (
+              {chat.members && chat.members.length === 2 ? (
                 <img
                   src={chat.members[0].avatar}
                   alt="friend avatar"
@@ -67,7 +150,7 @@ const ChatList = ({ selectChat }) => {
               )}
             </Col>
             <Col xs={8}>
-              {chat.members.length === 1 ? (
+              {chat.members && chat.members.length === 2 ? (
                 <p>
                   <strong>{chat.members[0].username}</strong>
                 </p>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -15,59 +15,17 @@ export const initialState = {
     password: "",
   },
   userInfo: {
-    _id: "6194e5dbdf63d1c9d6339f07",
+    _id: "",
     name: "",
     email: "",
     avatar: "",
   },
   chats: {
     active: {
-      _id: "6194e70efbecea49db16fae3",
-      members: [
-        {
-          _id: "6194e5dbdf63d1c9d6339f07",
-          username: "Cristina17",
-          email: "Jacklyn18@hotmail.com",
-          createdAt: "2021-11-17T11:22:03.074Z",
-          updatedAt: "2021-11-17T11:23:37.540Z",
-          socket: "bipqmfftd-iy_slYAAAB",
-        },
-      ],
-      name: "Test",
-      history: [
-        {
-          content: {
-            text: "Hello John!",
-          },
-          sender: {
-            username: "Cristina17",
-            email: "Jacklyn18@hotmail.com",
-            socket: "tBRz-wWxYibSbt89AAAB",
-            _id: "6194e5dbdf63d1c9d6339f07",
-            updatedAt: "2021-11-18T10:25:47.296Z",
-            createdAt: "2021-11-17T11:22:03.074Z",
-          },
-          _id: "61962a316c9733944e6b71ef",
-          updatedAt: "2021-11-18T10:25:53.462Z",
-          createdAt: "2021-11-18T10:25:53.462Z",
-        },
-        {
-          content: {
-            text: "Hello John!",
-          },
-          sender: {
-            username: "Cristina17",
-            email: "Jacklyn18@hotmail.com",
-            socket: "tBRz-wWxYibSbt89AAAB",
-            _id: "6194e5dbdf63d1c9d6339f07",
-            updatedAt: "2021-11-18T10:25:47.296Z",
-            createdAt: "2021-11-17T11:22:03.074Z",
-          },
-          _id: "61962a326c9733944e6b71fd",
-          updatedAt: "2021-11-18T10:25:54.524Z",
-          createdAt: "2021-11-18T10:25:54.524Z",
-        },
-      ],
+      _id: "",
+      members: [],
+      name: "",
+      history: [],
       image: "",
     },
     list: [],
